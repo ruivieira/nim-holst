ec782bde870e7b6eecfe16367a9b79bc7232bcd1
Rui Vieira <ruidevieira@googlemail.com>
2021-04-06 18:47:25 +0100

    Add image parsing

--- src/holst.nim
+++ src/holst.nim
@@ -1,5 +1,6 @@
 import json
 import sequtils
+import options
 
 type KernelSpec = object
   display_name*: string
@@ -16,6 +17,7 @@ type Cell = object
   kind*: CellKind
   source*: seq[string]
   outputs*: seq[string]
+  image_data*: Option[string]
 
 type JupyterNotebook = object
   metadata*: Metadata
@@ -37,6 +39,7 @@ proc read(path: string): JupyterNotebook =
   var cells = newSeq[Cell]()
   
   for cell in jsonNode["cells"]:
+    var image_data = none(string)
     let cell_type = cell["cell_type"].getStr
     let cell_source = cell["source"].elems.map(proc(x: JsonNode):string = x.getStr)
     if cell_type == "markdown":
@@ -44,28 +47,37 @@ proc read(path: string): JupyterNotebook =
       cells.add(c)
     elif cell_type == "code":
       # parsing the outputs of a code cell is not simple...
+      ## check if any of the output items has a key called "data"
+      let outputs = cell["outputs"]
+      let data = outputs.elems.filter(proc(output: JsonNode):bool = output.hasKey("data")).map(proc(x:JsonNode):JsonNode = x["data"])
+      if data.len > 0:
+        let images = data.filter(proc(x: JsonNode): bool = x.hasKey("image/png"))
+        if images.len > 0:
+        # if images.len >0:
+          image_data = some(images[0]["image/png"].getStr)
 
       # finally create the code cell
-      let c = Cell(kind: CellKind.Code, source: cell_source)
+      let c = Cell(kind: CellKind.Code, source: cell_source, image_data: image_data)
       cells.add(c)
 
   let notebook = JupyterNotebook(metadata: metadata,
                                  cells: cells)
 
-  return notebook
+  return notebook 
   
 
 when isMainModule:
   echo("hello")
-  let notebook = read("./RunningCode.ipynb")
+  let notebook = read("./SVM.ipynb")
 
   echo notebook.metadata.kernelspec.display_name
   echo notebook.metadata.kernelspec.language
   echo notebook.metadata.kernelspec.name
 
   for cell in notebook.cells:
-    echo(cell.kind)
-    echo($cell.source)
+    # echo(cell.kind)
+    # echo($cell.source)
+    echo(cell.image_data)
 
 
   # for cell in jsonNode["cells"]:
