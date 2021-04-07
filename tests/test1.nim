fe0242af22db68363e58ed70f223f73d5ef696d3
Rui Vieira <ruidevieira@googlemail.com>
2021-04-07 23:03:08 +0100

    Add initial unit tests

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,8 +1,27 @@
-# This is just an example to get you started. You may wish to put all of your
-# tests into a single file, or separate them into multiple `test1`, `test2`
-# etc. files (better names are recommended, just make sure the name starts with
-# the letter 't').
-#
-# To run these tests, simply execute `nimble test`.
-
 import unittest
+import sequtils
+import holst
+
+suite "Smoke tests":
+  setup:
+    let java_notebook = read("./tests/notebook.ipynb")
+
+  test "Get kernel name":
+    check(java_notebook.metadata.kernelspec.name == "java")
+
+  test "Get language name":
+    check(java_notebook.metadata.kernelspec.language == "java")
+
+  test "Get display name":
+    check(java_notebook.metadata.kernelspec.display_name == "Java")
+
+  test "Get total cells":
+    check(java_notebook.cells.len == 6)
+
+  test "Get code cells":
+    check(java_notebook.cells.filter(
+      proc(cell: Cell): bool = cell.kind == CellKind.Code).len == 5)
+
+  test "Get markdown cells":
+    check(java_notebook.cells.filter(
+      proc(cell: Cell): bool = cell.kind == CellKind.Markdown).len == 1)
