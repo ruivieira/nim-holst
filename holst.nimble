f8ba99773e927aa1dafb3297bce7cd7c80a5c891
Rui Vieira <ruidevieira@googlemail.com>
2021-04-08 19:58:24 +0100

    Fix image export markdown path

--- holst.nimble
+++ holst.nimble
@@ -1,6 +1,6 @@
 # Package
 
-version       = "0.1.2"
+version       = "0.1.3"
 author        = "Rui Vieira"
 description   = "A Jupyter notebook parser"
 license       = "AGPL-3.0-only"
--- tests/test1.nim
+++ tests/test1.nim
@@ -5,6 +5,10 @@ import holst
 suite "Smoke tests":
   setup:
     let java_notebook = read("./tests/notebook.ipynb")
+    var nb = read("./tests/notebook.ipynb")
+    nb.image_prefix = "notebook-img"
+    nb.image_dest = "./my_images"
+
 
   test "Get kernel name":
     check(java_notebook.metadata.kernelspec.name == "java")
@@ -25,3 +29,14 @@ suite "Smoke tests":
   test "Get markdown cells":
     check(java_notebook.cells.filter(
       proc(cell: Cell): bool = cell.kind == CellKind.Markdown).len == 1)
+
+  test "Default image export path and prefix":
+    check(java_notebook.image_dest == "./images")
+    check(java_notebook.image_prefix == "image")
+
+  test "Set image path and prefix":
+    check(nb.image_dest == "./my_images")
+    check(nb.image_prefix == "notebook-img")
+  
+  test "Get image full path":
+    check(nb.build_image_path(1) == "my_images/notebook-img-1.png")
